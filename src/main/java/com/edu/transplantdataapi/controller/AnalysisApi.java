8590e3c970ebab7d961ccbd95c4bf4cd141e0fe9
Julia <jplaczkowska6@gmail.com>
2022-05-20 21:55:16 +0200

    make Exceptions extend RuntimeException and add RuntimeExceptionHandler

--- src/main/java/com/edu/transplantdataapi/controller/AnalysisApi.java
+++ src/main/java/com/edu/transplantdataapi/controller/AnalysisApi.java
@@ -1,10 +1,6 @@
 package com.edu.transplantdataapi.controller;
 
-
 import com.edu.transplantdataapi.dto.analysis.ChiSquareTestParameters;
-import com.edu.transplantdataapi.exceptions.InvalidClassFactorNameException;
-import com.edu.transplantdataapi.exceptions.InvalidFactorNameException;
-import com.edu.transplantdataapi.exceptions.InvalidSignificanceValue;
 import com.edu.transplantdataapi.service.analysis.ChiSquareManager;
 import com.edu.transplantdataapi.service.transplantdata.SurvivalResultManager;
 import lombok.AllArgsConstructor;
@@ -25,15 +21,15 @@ public class AnalysisApi {
             @RequestParam String factor,
             @RequestParam String classFactor
     ) {
-            return ResponseEntity
-                    .ok(survivalResultsManager
-                            .getHistogramData(factor, classFactor));
+        return ResponseEntity
+                .ok(survivalResultsManager
+                        .getHistogramData(factor, classFactor));
     }
 
     @GetMapping("api/survivalResult/chisquaretest")
     public ResponseEntity<?> getChiSquareTestResult(
             @RequestBody ChiSquareTestParameters parameters
     ) {
-            return ResponseEntity.ok(chiSquareManager.getChiSquareTestResult(parameters));
+        return ResponseEntity.ok(chiSquareManager.getChiSquareTestResult(parameters));
     }
 }
